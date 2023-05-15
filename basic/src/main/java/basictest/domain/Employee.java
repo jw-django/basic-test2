5089d57ccfd72c86ebb40f3357f3e35c8c89e6e5
jw-dev <81397544+jw-django@users.noreply.github.com>
2023-05-15 16:30:43 +0900

    commit

---
+++ basic/src/main/java/basictest/domain/Employee.java
@@ -0,0 +1,56 @@
+package basictest.domain;
+
+import basictest.BasicApplication;
+import basictest.domain.EmployeeAdded;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import org.springframework.context.ApplicationContext;
+
+@Entity
+@Table(name = "Employee_table")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userId;
+
+    private Boolean isManager;
+
+    @Embedded
+    @AttributeOverride(
+        name = "id",
+        column = @Column(name = "departmentId", nullable = true)
+    )
+    private DepartmentId departmentId;
+
+    @Embedded
+    @AttributeOverride(
+        name = "id",
+        column = @Column(name = "rankId", nullable = true)
+    )
+    private RankId rankId;
+
+    @ElementCollection
+    private List<EmployeeUser> employeeUser;
+
+    @PostPersist
+    public void onPostPersist() {
+        EmployeeAdded employeeAdded = new EmployeeAdded(this);
+        employeeAdded.publishAfterCommit();
+    }
+
+    public static EmployeeRepository repository() {
+        EmployeeRepository employeeRepository = applicationContext()
+            .getBean(EmployeeRepository.class);
+        return employeeRepository;
+    }
+
+    public static ApplicationContext applicationContext() {
+        return BasicApplication.applicationContext;
+    }
+}
